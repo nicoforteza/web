cf3b013c4a32af8157f502a42c422d9a2c2ef206
Nico Forteza <nicoforteza@gmail.com>
2023-05-09 22:08:50 +0200

    fix paths 3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
+// next.config.js
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 module.exports = {
+  assetPrefix: "/web/",
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config) => {
