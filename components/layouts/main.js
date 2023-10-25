48ab233c41559f537d342ffaf4200662e65c7e42
Nico Forteza <nicoforteza@gmail.com>
2023-10-25 13:40:20 +0200

    more seo

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,16 +9,9 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={12}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Nico's homepage" />
         <meta name="author" content="Nicolás Forteza" />
         <link rel="apple-touch-icon" href="icons8-mate-office-32.png" />
         <link rel="shortcut icon" href="/icons8-mate-office-16.png" type="image/x-icon" />
-        <meta name="twitter:title" content="Nicolás Forteza" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@nicoforteza1" />
-        <meta name="twitter:creator" content="@nicoforteza1" />
-        <meta property="og:site_name" content="Nicolás Forteza" />
-        <meta name="og:title" content="Nicolás Forteza" />
         <meta property="og:type" content="website" />
         <title>Nico Forteza Web</title>
         <meta
