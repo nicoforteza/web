1f682cbf8f1b084b50cc0da155af1b2f30b2b1be
Nico Forteza <nicoforteza@gmail.com>
2026-02-14 22:10:18 +0100

    fix years

--- components/bio.js
+++ components/bio.js
@@ -2,11 +2,23 @@ import { Box } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
 export const BioSection = styled(Box)`
-  paddingLeft: 3.4em;
-  text-indent: -3.4em;
+  padding-left: 5.5em;
+  text-indent: -5.5em;
+
+  @media (max-width: 480px) {
+    padding-left: 0;
+    text-indent: 0;
+    margin-bottom: 0.8em;
+  }
 `
 
 export const BioYear = styled.span`
   font-weight: bold;
   margin-right: 1em;
+
+  @media (max-width: 480px) {
+    display: block;
+    margin-right: 0;
+    margin-bottom: 0.2em;
+  }
 `
